9a980107fbbb67444353ca0b64af1ce3132fc754
srilaxmi-1992 <srilaxmilucky123@gmail.com>
2026-06-04 21:20:26 -0400

    config changes

--- config/config.ts
+++ config/config.ts
@@ -1,5 +1,6 @@
 export const config = {
     baseURL: 'http://localhost/opencart/upload/index.php',
     email: 'yuvikarao@abc.com',
-    password: 'test@123'
+    password: 'test@123',
+    apiBaseURL : "https://restful-booker.herokuapp.com"
 };
