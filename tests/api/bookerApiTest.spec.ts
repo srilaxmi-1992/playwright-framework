10b9c34e038599e5bac0f91e4915b2e2d94927cc
srilaxmi-1992 <srilaxmilucky123@gmail.com>
2026-06-04 21:17:37 -0400

    added api test cases

---
+++ tests/api/bookerApiTest.spec.ts
@@ -0,0 +1,124 @@
+import { expect, test } from "@playwright/test";
+import { config } from "../../config/config";
+import {  BookingUtils } from '../../utils/BookingUtils';
+import { BookingDataUtils } from "../../utils/BookingDataUtils";
+
+let token: string = ""
+test.beforeEach('Login To API Tests Generate Token', async ({ request }) => {
+
+    const response = await request.post(config.apiBaseURL + '/auth', {
+        data: {
+            "username": "admin",
+            "password": "password123"
+        },
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+
+    const responseBody = await response.json()
+    expect(response.ok()).toBeTruthy()
+    expect(response.status()).toBe(200)
+    token = responseBody.token
+    console.log(`Token is generated ${token}`)
+
+})
+
+test('Create Booking', { tag : '@api'},async ({ request }) => {
+
+    const bookingData =
+        BookingDataUtils.getBooking('TC_API_001');
+
+    const response = await request.post(
+        `${config.apiBaseURL}/booking`,
+        {
+            data: bookingData,
+            headers: {
+                'Content-Type': 'application/json',
+                 Accept: 'application/json'
+            }
+        }
+    );
+
+    expect(response.status()).toBe(200);
+    expect(response.ok()).toBeTruthy();
+
+    const responseBody = await response.json();
+    expect(responseBody.bookingid).toBeTruthy();
+    expect(responseBody.booking).toMatchObject(bookingData);
+});
+
+test('Get Booking', { tag : '@api'},async ({ request }) => {
+
+  const bookingData = BookingDataUtils.getBooking('TC_API_001');
+
+  const bookingId = await BookingUtils.createBooking(
+      request,
+      bookingData
+    );
+
+  const response = await request.get(
+    `${config.apiBaseURL}/booking/${bookingId}`
+  );
+
+  expect(response.status()).toBe(200);
+
+  const responseBody = await response.json();
+
+  expect(responseBody).toMatchObject(bookingData);
+});
+
+test('Update Booking', { tag : '@api'}, async ({ request }) => {
+
+    const createBookingData =
+        BookingDataUtils.getBooking('TC_API_001');
+
+    const updateBookingData =
+        BookingDataUtils.getBooking('TC_API_002');
+
+    const bookingId = await BookingUtils.createBooking(
+        request,
+        createBookingData
+    );
+
+    const response = await request.put(
+        `${config.apiBaseURL}/booking/${bookingId}`,
+        {
+            headers: {
+                 Cookie: `token=${token}`,
+                'Content-Type': 'application/json',
+                 Accept: 'application/json'
+            },
+            data: updateBookingData
+        }
+    );
+
+    expect(response.status()).toBe(200);
+
+    const responseBody = await response.json();
+
+    expect(responseBody).toMatchObject(updateBookingData);
+});
+
+test('Delete Booking', { tag : '@api'},async ({ request }) => {
+
+    const bookingData =
+        BookingDataUtils.getBooking('TC_API_001');
+
+    const bookingId = await BookingUtils.createBooking(
+        request,
+        bookingData
+    );
+
+    const response = await request.delete(
+        `${config.apiBaseURL}/booking/${bookingId}`,
+        {
+            headers: {
+                Cookie: `token=${token}`
+            }
+        }
+    );
+
+    expect(response.status()).toBe(201);
+
+});
---
+++ utils/BookingUtils.ts
@@ -0,0 +1,25 @@
+import { APIRequestContext, expect } from '@playwright/test';
+import { config } from '../config/config';
+import { BookingData } from './BookingDataUtils';
+
+export class BookingUtils {
+
+    static async createBooking(
+        request: APIRequestContext, bookingData: BookingData) {
+
+        const response = await request.post(
+            `${config.apiBaseURL}/booking`,
+            {
+                data: bookingData,
+                headers: {
+                    'Content-Type': 'application/json',
+                    Accept: 'application/json'
+                }
+            }
+        );
+
+        expect(response.status()).toBe(200);
+        const responseBody = await response.json();
+        return responseBody.bookingid;
+    }
+}
